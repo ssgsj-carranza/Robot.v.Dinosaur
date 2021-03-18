0b07f0d7e95a5f8f70578d0f4de80166cbd22fb7
unknown <ssgsj.carranza@gmail.com>
2021-03-18 12:07:10 -0500

    created classes for robot, dino, fleet, herd, weapon, battlefield

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
+from robot_dino import Robot
+from robot_dino import Dinosaur
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    RobotOne = Robot()
+    RobotTwo = Robot()
+    RobotThree = Robot()
+    DinoOne = Dinosaur()
+    DinoTwo = Dinosaur()
+    DinoThree = Dinosaur()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
---
+++ robot_dino.py
@@ -0,0 +1,17 @@
+class Robot:
+    def __init__(self):
+        self.name = "voltron"
+        self.health = "250"
+        self.power_level = "400"
+        self.weapon = "sword"
+        self.attack_power = "50"
+class Dinosaur:
+    def __init__(self):
+        self.type = "Metal-Greymon"
+        self.health = "180"
+        self.energy = "500"
+        self.attack_power = "100"
+class Fleet:
+class Herd:
+class Weapon:
+class Battlefield:
