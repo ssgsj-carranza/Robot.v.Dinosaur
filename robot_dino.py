6e160915ee878cc8af2223da6b8e704c9f8b6733
unknown <ssgsj.carranza@gmail.com>
2021-03-18 12:32:12 -0500

    added method to know when robots health reaches 0

--- robot_dino.py
+++ robot_dino.py
@@ -12,6 +12,40 @@ class Dinosaur:
         self.energy = "500"
         self.attack_power = "100"
 class Fleet:
+    def __init__(self):
+        self.robots = list()
+        self.robots.append(Robot())
+        self.robots.append(Robot())
+        self.robots.append(Robot())
+    def defeated(self):
+        for robot in self.robots:
+            if robot.health > 0:
+                return False
+        return True
 class Herd:
-class Weapon:
-class Battlefield:
+    def __init__(self):
+        self.dinosaurs = list()
+        self.dinosaurs.append(Dinosaur())
+        self.dinosaurs.append(Dinosaur())
+        self.dinosaurs.append(Dinosaur())
+
+# class Weapon:
+# class Battlefield:
+
+def fightSequence():
+    robot = Fleet()
+    dinosaur = Herd()
+    encounter = 1
+    turn = 'Dinosaur'
+    while encounter == 1:
+        if turn == 'Dinosaur':
+            action = input("What would you like to do (Attack)? ")
+            if action == 'Attack':
+                encounter = humanAttack(Player)
+                turn = 'monster'
+        elif turn == 'monster':
+            encounter = monsterAttack(Monster)
+            turn = 'player'
+
+
+fightSequence()
